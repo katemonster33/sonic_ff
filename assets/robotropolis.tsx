e87affcb8bdd69be64d656116fe05caea9169145
Katie M <mckeanke@msu.edu>
2025-01-19 20:48:43 -0500

    more sprites. first stab at using tiled to make a map

---
+++ assets/robotropolis.tsx
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="robotropolis" tilewidth="160" tileheight="160" tilecount="27" columns="0" fillmode="preserve-aspect-fit">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <tile id="0">
+  <image source="../sprites/robotropolis/wall_blue.png" width="16" height="16"/>
+ </tile>
+ <tile id="1">
+  <image source="../sprites/robotropolis/wall_corner.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../sprites/robotropolis/wall_dark.png" width="160" height="96"/>
+ </tile>
+ <tile id="3">
+  <image source="../sprites/robotropolis/wall_edge.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../sprites/robotropolis/wall_ground_angled_right.png" width="64" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../sprites/robotropolis/wall_ground_angled.png" width="64" height="16"/>
+ </tile>
+ <tile id="6">
+  <image source="../sprites/robotropolis/wall_ground_edge.png" width="16" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="../sprites/robotropolis/wall_ground.png" width="16" height="32"/>
+ </tile>
+ <tile id="8">
+  <image source="../sprites/robotropolis/wall_pipe.png" width="80" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="../sprites/robotropolis/wall.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="../sprites/robotropolis/vent_pipe_vert.png" width="16" height="16"/>
+ </tile>
+ <tile id="11">
+  <image source="../sprites/robotropolis/vent.png" width="64" height="160"/>
+ </tile>
+ <tile id="12">
+  <image source="../sprites/robotropolis/wall_angled.png" width="64" height="16"/>
+ </tile>
+ <tile id="13">
+  <image source="../sprites/robotropolis/boulder.png" width="32" height="32"/>
+ </tile>
+ <tile id="14">
+  <image source="../sprites/robotropolis/fan_right.png" width="64" height="96"/>
+ </tile>
+ <tile id="15">
+  <image source="../sprites/robotropolis/fan.png" width="64" height="96"/>
+ </tile>
+ <tile id="16">
+  <image source="../sprites/robotropolis/fence.png" width="48" height="80"/>
+ </tile>
+ <tile id="17">
+  <image source="../sprites/robotropolis/foreground_wall_blue.png" width="16" height="16"/>
+ </tile>
+ <tile id="18">
+  <image source="../sprites/robotropolis/foreground_wall_left.png" width="48" height="16"/>
+ </tile>
+ <tile id="19">
+  <image source="../sprites/robotropolis/foreground_wall_side_left.png" width="48" height="48"/>
+ </tile>
+ <tile id="20">
+  <image source="../sprites/robotropolis/ground_yellow.png" width="48" height="32"/>
+ </tile>
+ <tile id="21">
+  <image source="../sprites/robotropolis/ground.png" width="32" height="32"/>
+ </tile>
+ <tile id="22">
+  <image source="../sprites/robotropolis/junk_pile.png" width="32" height="32"/>
+ </tile>
+ <tile id="23">
+  <image source="../sprites/robotropolis/rock.png" width="32" height="16"/>
+ </tile>
+ <tile id="24">
+  <image source="../sprites/robotropolis/sewer_grate.png" width="64" height="32"/>
+ </tile>
+ <tile id="25">
+  <image source="../sprites/robotropolis/smallrocks.png" width="32" height="32"/>
+ </tile>
+ <tile id="26">
+  <image source="../sprites/robotropolis/vent_left_pipe.png" width="16" height="64"/>
+ </tile>
+</tileset>
